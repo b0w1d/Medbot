b1457ba0be491f1238851f93327d2fa287282beb
b0w1d <61a5t0@gmail.com>
2018-02-01 15:10:17 +0800

    Fixed typo

--- app.rb
+++ app.rb
@@ -331,7 +331,7 @@ class Processor
       bar: %w(bar group groups grouped grouping categorize categorizes categorized categorizing)
     } .each do |graph_type, keys|
       if words.any? { |w| keys.include?(w) }
-        graph = send("#{graph_type.capitalize}Graph.new", message: @msg, filter: $filter_info)
+        graph = send("#{graph_type.capitalize}Graph.new", message: @msg, filter: @filter)
         return graph.get_link_of_image
       end
     end
