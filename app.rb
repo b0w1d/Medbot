33ecd5353fae142b357e63747a77de4d94ca48ee
b0w1d <61a5t0@gmail.com>
2018-03-12 16:12:25 +0800

    .

--- app.rb
+++ app.rb
@@ -152,7 +152,8 @@ class Graph
 
   def is_useful?(word:)
     return 0 if word.size == 1
-    return 0 if %w(he his him boy man male she her girl lady female ml dl mmol item and with of to for was on the mg time or is are they them their doctor hospital in no under below above status at days without).include?(word.downcase)
+    return 0 if word.include?(/[^a-zA-Z]/)
+    return 0 if %w(and with of he his him boy man male she her girl lady female ml dl mmol item and with of to for was on the mg time or is are they them their doctor hospital in no under below above status at days without).include?(word.downcase)
     /[^a-zA-Z]/.match?(word) ? 0 : 1
   end
 
